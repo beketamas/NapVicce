43e69f0d366046c0f593b3e96bae4b10194f16b3
valami <valami>
2024-10-07 13:53:05 +0200

    Add project files.

---
+++ NapVicce/Controllers/NapVicceController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NapVicce.Controllers
+{
+    public class NapVicceController : Controller
+    {
+        Dictionary<string, string> viccikek = new Dictionary<string, string>()
+        {
+            {"Monday" , "xd"} ,
+            {"Tuseday", "lol"} ,
+            {"Wednesday", "why"} ,
+            {"Thursday", "kekw"} ,
+            {"Friday", "ok"} ,
+            {"Saturday", "no"} ,
+            {"Sunday", "yes"} 
+        };
+        string date = DateTime.Now.DayOfWeek.ToString();
+        [HttpGet]
+        [Route("[controller]")]
+
+        public String Get() => $"{viccikek.Where(x => x.Key == date).First().Key} : {viccikek.Where(x => x.Key == date).First().Value}";
+    }
+}
